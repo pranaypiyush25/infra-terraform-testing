3d2d58c27c42b144fe96fd618e936e6c28bc4dd2
Sai Krishna Palakollu <palakollusaikrishna@gmail.com>
2023-08-10 18:23:13 +0530

    Update main.tf

--- env_v2/core/core/ap-south-1/iam_role/main.tf
+++ env_v2/core/core/ap-south-1/iam_role/main.tf
@@ -10,5 +10,9 @@ provider "aws" {
 
 module "iamrole" {
   source = "/home/ec2-user/code-sai-test/infra-terraform//modules_v2/iam_roles"
-
+  env                   = local.account_vars.locals.env
+  region                = local.account_vars.locals.region
+  global_tags           = local.account_vars.locals.global_tags
+  create_app_env_bucket = local.account_vars.locals.iam_role.create_app_env_bucket
+  service_linked_role_suffix = local.account_vars.locals.env
 }
